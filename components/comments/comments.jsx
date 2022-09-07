e8236e879bff091879c9df40f165d7c0094c2589
xgmak94 <xgmak94@gmail.com>
2022-09-06 20:49:49 -0700

    Moved firebase config to .env.local file

--- components/comments/comments.jsx
+++ components/comments/comments.jsx
@@ -1,4 +1,3 @@
-import config from '../../config.js';
 import react, { useState } from 'react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
@@ -8,7 +7,15 @@ import { getAnalytics } from 'firebase/analytics';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
-const firebaseConfig = config;
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_API_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
 
 const app = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
@@ -18,6 +25,7 @@ let analytics;
 if (app.name && typeof window !== 'undefined') {
   analytics = getAnalytics(app);
 }
+
 
 function Comments() {
   const messagesRef = firestore.collection('comments');
