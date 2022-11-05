ed12dcdb9e5d8ef04f69addac1452f6a2c8908a6
xgmak94 <xgmak94@gmail.com>
2022-11-05 14:46:20 -0700

    Fixing resume

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -14,7 +14,7 @@ export default function Navbar() {
           <NavItem link="/projects" text="projects" />
           <NavItem link="/about" text="about" />
           <NavItem link="/contact" text="contact" />
-          <NavItem link="/resume.pdf" text="resume" />
+          <NavItem link="/Gary%20Mak%20resume.pdf" text="resume" />
           <ModeToggle />
         </div>
       </div>
