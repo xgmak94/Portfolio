d352980d2bc4a50129542ccd3aa958fd2714cdb2
xgmak94 <xgmak94@gmail.com>
2022-11-06 17:17:12 -0800

    Fixed navbar on small screens

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -11,32 +11,32 @@ export default function Navbar() {
   const [menu, setMenu] = useState(false);
 
   return (
-    <nav className="p-3 text-black dark:text-white bg-gray-500 dark:bg-gray-700 border-b border-white dark:border-black">
+    <motion.nav
+      className="p-3 text-black dark:text-white bg-gray-500 dark:bg-gray-700 border-b border-white dark:border-black"
+      initial={{
+        y: -500,
+      }}
+      animate={{
+        y: 0,
+      }}
+      transition={{
+        duration: 2,
+      }}
+    >
       <div className="flex flex-row justify-between items-center gap-3">
         <div className="container flex gap-3">
           <NavItem link="/" text="home" setMenu={setMenu} />
         </div>
-        <motion.div
-          className="hidden md:flex gap-3"
-          initial={{
-            y: -500,
-          }}
-          animate={{
-            y: 0,
-          }}
-          transition={{
-            duration: 2,
-          }}
-        >
+        <div className="hidden md:flex gap-3">
           <NavMenu setMenu={setMenu} />
-          <button
-            className="flex md:hidden hover:bg-gray-300 dark:hover:bg-gray-600 rounded-xl text-lg p-2 text-center self-center"
-            onClick={() => setMenu((prev) => !prev)}
-          >
-            {menu ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
-          </button>
-          <ModeToggle />
-        </motion.div>
+        </div>
+        <button
+          className="flex md:hidden hover:bg-gray-300 dark:hover:bg-gray-600 rounded-xl text-lg p-2 text-center self-center"
+          onClick={() => setMenu((prev) => !prev)}
+        >
+          {menu ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
+        </button>
+        <ModeToggle />
       </div>
       <AnimatePresence mode="wait">
         {menu ? (
@@ -59,6 +59,6 @@ export default function Navbar() {
           </motion.div>
         ) : null}
       </AnimatePresence>
-    </nav>
+    </motion.nav>
   );
 }
