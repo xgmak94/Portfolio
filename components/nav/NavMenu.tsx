cdd200b726d9364f993d76592578741740e98f2a
xgmak94 <xgmak94@gmail.com>
2022-12-18 17:39:23 -0800

    resume update

--- components/nav/NavMenu.tsx
+++ components/nav/NavMenu.tsx
@@ -32,14 +32,7 @@ export default function NavMenu({ menu, setMenu }: props) {
             <NavItem link="/projects" text="projects" setMenu={setMenu} />
             <NavItem link="/about" text="about" setMenu={setMenu} />
             <NavItem link="/contact" text="contact" setMenu={setMenu} />
-            <Link href="/resume.pdf" locale={false} rel="noopener noreferrer">
-              <motion.button
-                className="rounded-xl p-2 text-xl capitalize hover:bg-teal-500 dark:hover:bg-gray-600"
-                whileHover={{ scale: 1.05 }}
-              >
-                resume
-              </motion.button>
-            </Link>
+            <NavItem link="/resume" text="resume" setMenu={setMenu} />
           </motion.div>
         ) : null}
       </AnimatePresence>
